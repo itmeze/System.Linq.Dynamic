69137732af54b5eb85d93f9da14dc89f8f31ceab
ITmeze <michal.zygula@gmail.com>
2014-01-30 17:16:40 +0100

    fixing coordinates

--- Test/System.Linq.Dynamic.Tests/DbGeographyTests.cs
+++ Test/System.Linq.Dynamic.Tests/DbGeographyTests.cs
@@ -9,9 +9,9 @@ namespace System.Linq.Dynamic.Tests
 		[Fact]
 		public void DistanceSearchShouldWork()
 		{
-			var warsaw = new Town() {Name = "Warsaw", Location = DbGeography.FromText("POINT(52.14 21.1)")};
-			var lodz = new Town() {Name = "Lodz", Location = DbGeography.FromText("POINT(51.47 19.28)")};
-			var berlin = new Town() {Name = "Berlin", Location = DbGeography.FromText("POINT(52.31 13.23)")};
+			var warsaw = new Town() { Name = "Warsaw", Location = DbGeography.FromText("POINT(52.229718 21.012214)") };
+			var lodz = new Town() { Name = "Lodz", Location = DbGeography.FromText("POINT(51.759304 19.455969)") };
+			var berlin = new Town() { Name = "Berlin", Location = DbGeography.FromText("POINT(52.523206 13.410836)") };
 			var towns = new List<Town>
 			            {
 				            warsaw,
@@ -19,7 +19,11 @@ namespace System.Linq.Dynamic.Tests
 				            berlin
 			            }.AsQueryable();
 
-			Assert.Equal(1, towns.Where("Location.Distance(@0) < 200000", warsaw.Location).Count());
+			var distance = warsaw.Location.Distance(lodz.Location);
+
+			Assert.Equal(1, towns.Where("Location.Distance(@0) < 20000", warsaw.Location).Count());
+			Assert.Equal(2, towns.Where("Location.Distance(@0) < 200000", warsaw.Location).Count());
+			Assert.Equal(3, towns.Where("Location.Distance(@0) < 2000000", warsaw.Location).Count());
 		}
 	}
 }
